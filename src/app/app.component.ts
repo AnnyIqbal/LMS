cf99cbff3b9dca321c9a6678ef22621c5618bcfe
Anny <quratulann_iqbal@hotmail.com>
2016-12-28 14:14:03 +0500

    ui updation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
-  MenuIcons: string[] = ['MENU', 'SEARCH', 'USER', 'BOOK', 'LIBRARY'];
+  MenuIcons: string[] = ['MENU', 'HOME', 'SEARCH', 'USER', 'BOOK', 'ISSUE A BOOK', 'RATE A BOOK', 'BOOK SHELF', 'LIBRARY', 'LOCATOR'];
+  icons: string[] = [
+    'home', 'search', 'person', 'book', 'add_shopping_cart', 'favorite', 'shopping_cart', 'account_balance', 'location_on'
+  ];
 }
